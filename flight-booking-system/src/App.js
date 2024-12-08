3fd105d9a65b6b577b4bda68af937e56703ab993
Sanika2431 <khamitkarsanika12gmail.com>
2024-12-08 22:00:00 +0530

    Project Added

---
+++ flight-booking-system/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FBSRouter from "./routing/FBSRouter";
+
+function App() {
+  return (
+    <div className="container">
+      <FBSRouter />
+    </div>
+  );
+}
+
+export default App;
